ce45b3ac8063f41feb4a8e6023d1ceae8068e8ac
communitytrustcollective <community.trust.collective@gmail.com>
2026-09-06 18:52:33 -0400

    Add files via upload

--- community_trust_collective.py
+++ community_trust_collective.py
@@ -81,7 +81,7 @@ This tool helps organizers identify potential safety concerns by securely sharin
 
 International privacy laws make sharing sensitive allegations difficult; we minimize the information stored to reduce legal risk: only whether reports exist and who to contact for more details.
 
-Information in this system is provided for community safety purposes and does not constitute a finding of guilt or wrongdoing. A record or match should be treated as a reason for organizers to seek additional information and make their own informed decisions. Users are responsible for using and handling information in accordance with applicable laws and privacy requirements.''')
+Information in this system is provided for community safety purposes and does not constitute a finding of guilt or wrongdoing. A record should be treated as a reason for organizers to seek additional information and make their own informed decisions. Users are responsible for using and handling information in accordance with applicable laws and privacy requirements.''')
 
 
 
