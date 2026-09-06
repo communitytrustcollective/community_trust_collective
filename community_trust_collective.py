e28c30c20e07a9e6ae76e12a09e19baf446e9c01
communitytrustcollective <community.trust.collective@gmail.com>
2026-09-06 19:01:43 -0400

    Add files via upload

--- community_trust_collective.py
+++ community_trust_collective.py
@@ -73,11 +73,11 @@ with right:
 
 
 st.text('')
-st.markdown('''**:blue-background[highlightProblem -]** Individuals with a history of misconduct may move between communities to where organizers are unaware of their history. 
+st.markdown('''**:blue-background[Problem -]** Individuals with a history of misconduct may move between communities to where organizers are unaware of their history. 
 
-**Solution -** This tool helps organizers identify potential safety concerns by securely sharing limited information about reported misconduct and, where appropriate, allows them to contact the original community for additional context.
+**:blue-background[Solution -]**This tool helps organizers identify potential safety concerns by securely sharing limited information about reported misconduct and, where appropriate, allows them to contact the original community for additional context.
 
-**Challenges -** International privacy laws make sharing sensitive allegations difficult; we minimize the information stored to reduce legal risk: only whether reports exist and who to contact for more details.
+**:blue-background[Challenges -]** International privacy laws make sharing sensitive allegations difficult; we minimize the information stored to reduce legal risk: only whether reports exist and who to contact for more details.
 
 **Helping communities share safety information**
 
