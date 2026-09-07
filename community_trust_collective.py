76485bf6497f7f6523254602c4bdce90e4426efe
communitytrustcollective <community.trust.collective@gmail.com>
2026-09-07 19:53:59 -0400

    Add files via upload

--- community_trust_collective.py
+++ community_trust_collective.py
@@ -424,7 +424,8 @@ st.text('')
 
 to_find = st.text_area('''**Filter Safety Records:**
 
-Paste an attendee list, names, or WSDC numbers (one per line) to filter down to ***potentially*** relevant reports. There may be false positives, please verify that matches refer to the correct individual.''', placeholder='''Name or WSDC number
+Check an attendee list for matches by pasting names or WSDC number (one per line)
+to filter down to ***potentially*** relevant reports. There may be false positives, please verify that matches refer to the correct individual.''', placeholder='''Name or WSDC number
 Name2 or WSDC number
 Name3 or WSDC number''')
 
