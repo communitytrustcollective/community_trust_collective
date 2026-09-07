b77514d2b8a7be7ff480cffbd731af76a993bb3d
communitytrustcollective <community.trust.collective@gmail.com>
2026-09-07 02:52:18 -0400

    Add files via upload

--- community_trust_collective.py
+++ community_trust_collective.py
@@ -380,8 +380,8 @@ Dance communities are highly interconnected, and people frequently travel betwee
 
 
 
-access_code = st.markdown(
-    "#### Secure Access Token",
+access_code = st.text_input(
+    "**Enter Secure Access Token**",
     type="password",
 )
 
